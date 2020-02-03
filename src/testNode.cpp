8f5b83bf360c1c7559e741aa331efac02a6382d4
Yulun Tian <yulun@mit.edu>
2020-02-02 22:09:50 -0500

    test using dpgo library

--- src/testNode.cpp
+++ src/testNode.cpp
@@ -1,10 +1,14 @@
 #include "distributed/PGOAgent.h"
 #include <ros/ros.h>
 
+using namespace DPGO;
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "test_node");
 	ros::NodeHandle nh;
+
+	PGOAgentParameters options(3,5,ROPTALG::RTR,true);
+	PGOAgent agent(0, options);
 
 	ros::spin();
 
